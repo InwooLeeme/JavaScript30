11399e064c2dfc74436c78bb4cda096bca3c2410
ItsmeLee <arsmagica90@gmail.com>
2021-01-10 19:45:46 +0900

    Rotate hour and minute bars
    Hour and Minute bars are rotate right on time.

--- Chapter2(CSS+JS Clock)/index.js
+++ Chapter2(CSS+JS Clock)/index.js
@@ -9,7 +9,11 @@ function getCurrentTime(){
     const hour = date.getHours();
     const minute = date.getMinutes();
     const seconds = date.getSeconds();
+    const tranHourDeg = (hour * 30) + 90;
+    const transMinDeg = (minute * 6) + 90;
     const transSecDeg = (seconds * 6) + 90;
+    hourBar.style.transform = `rotate(${tranHourDeg}deg)`;
+    minuteBar.style.transform = `rotate(${transMinDeg}deg)`;
     secondBar.style.transform = `rotate(${transSecDeg}deg)`;
 }
 
