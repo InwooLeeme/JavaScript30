0ea1deb9ff1b392c0c296414026448751898b4d6
ItsmeLee <arsmagica90@gmail.com>
2021-01-11 13:02:34 +0900

    Image Change Blur function
    When user scrolling the blur controller,
    the image blur getting bigger.

--- Chapter3(Playing with CSS Variables and JS)/chapter3.js
+++ Chapter3(Playing with CSS Variables and JS)/chapter3.js
@@ -4,21 +4,22 @@ const blurController = document.getElementById('blur');
 const colorController = document.getElementById('base');
 const image = document.querySelector('img');
 
-
+// Handling Spacing
 function handleSpace(){
     const currentSpace = spaceController.value;
     console.log(currentSpace);
     image.style.margin = `${parseInt(currentSpace)}px`;
 }
 
+// Handling Image Blur
 function handleBlurStyle(){
     const currentInputValue = blurController.value;
-    
+    image.style.filter = `blur(${parseInt(currentInputValue)}px)`;
 }
 
 function init(){
-    spaceController.addEventListener('change', handleSpace);
-    blurController.addEventListener('change',handleBlurStyle);
+    spaceController.addEventListener('input', handleSpace);
+    blurController.addEventListener('input',handleBlurStyle);
 }
 
 init();
