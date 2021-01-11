637db4be8558027c460638dd41154c7b3f9ff165
ItsmeLee <arsmagica90@gmail.com>
2021-01-11 12:41:36 +0900

    Control Spacing in image
    Control margin in image using spacing controller.
    The user change the spacing input value,
    the image margin change immediately.

--- Chapter3(Playing with CSS Variables and JS)/chapter3.js
+++ Chapter3(Playing with CSS Variables and JS)/chapter3.js
@@ -2,10 +2,13 @@
 const spaceController = document.getElementById('spacing');
 const blurController = document.getElementById('blur');
 const colorController = document.getElementById('base');
+const image = document.querySelector('img');
+
 
 function handleSpace(){
     const currentSpace = spaceController.value;
     console.log(currentSpace);
+    image.style.margin = `${parseInt(currentSpace)}px`;
 }
 
 function init(){
